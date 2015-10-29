4c0a2fd6c6d1c1e34f4e787a5b8dd221117d1a17
Bernard Mordan <bernardmordan@gmail.com>
2015-10-29 08:54:00 +0000

    adds clientside mouse streaming

--- src/index.js
+++ src/index.js
@@ -5,9 +5,17 @@ var client = new Nes.Client('ws://localhost:3000')
 client.connect(function (err) {
   if (err) return console.error(err)
 
-  client.request('connected', function (err, payload) {
-    if (err) return console.error(err)
-    console.log(payload)
-  })
+  var opts = {
+    path: '/connect',
+    method: 'POST',
+    headers: {},
+    payload: {x:window.innerWidth/2, y:window.innerHeight/2}
+  }
+
+  document.onmousemove = function (e) {
+    opts.payload.x = e.pageX
+    opts.payload.y = e.pageY
+    client.request(opts, function (err, res) {if (err) return console.error(err)})
+  }
 
 })
